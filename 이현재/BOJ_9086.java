e33536330e3d4c1c4568d08acae1427abdee277b
Devlee <hjcloud@me.com>
2024-06-16 17:44:05 +0900

    이현재 6월 2주차 코딩테스트 스터디 1.baekjoon_9086 2.baekjoon_11654 3.baekjoon_11720

---
+++ 이현재/BOJ_9086.java
@@ -0,0 +1,52 @@
+package 이현재;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_9086 {
+
+    /**
+     * 1546번: 문자열 - 120ms
+     * <pre>
+     *     <b>[문제]</b>
+     *     문자열을 입력으로 주면 문자열의 첫 글자와 마지막 글자를 출력하는 프로그램을 작성하시오.
+     *
+     *     <b>[입력]</b>
+     *     입력의 첫 줄에는 테스트 케이스의 개수 T(1 ≤ T ≤ 10)가 주어진다.
+     *     각 테스트 케이스는 한 줄에 하나의 문자열이 주어진다.
+     *     문자열은 알파벳 A~Z 대문자로 이루어지며 알파벳 사이에 공백은 없으며 문자열의 길이는 1000보다 작다.
+     *
+     *     <b>[출력]</b>
+     *     각 테스트 케이스에 대해서 주어진 문자열의 첫 글자와 마지막 글자를 연속하여 출력한다.
+     * </pre>
+     *
+     * @param args args
+     * @throws IOException IOException
+     * @see <a href="https://www.acmicpc.net/problem/9086">문제 보기</a>
+     */
+    public static void main00(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            char[] str = br.readLine().toCharArray();
+            sb.append(str[0]).append(str[str.length - 1]).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    /**
+     * charAt 을 활용한 풀이 - 148ms
+     * @param args args
+     * @throws IOException IOException
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        while(n-- > 0) {
+            String str = br.readLine();
+            System.out.println(str.charAt(0) + "" + str.charAt(str.length()-1));
+        }
+    }
+}
--- 이현재/test.java
+++ 이현재/test.java
@@ -1,31 +1,19 @@
 package 이현재;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class test {
 
     public static void main(String[] args) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        int n = read();
-        int[] lst = new int[n];
-        for (int i = 0; i < n; i++) lst[i] = i + 1;
-        int m = read();
-        for (int j = 0; j < m; j++) {
-            int a = read() - 1;
-            int b = read() - 1;
-            int cnt = (b - a + 1) / 2;
-            for (int l = 0; l < cnt; l++) {
-                int tmp = lst[a + l];
-                lst[a + l] = lst[b - l];
-                lst[b - l] = tmp;
-            }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+        char[] str = br.readLine().toCharArray();
+        int result = 0;
+        for (char c : str) {
+            result += Integer.parseInt(String.valueOf(c));
         }
-        for (int k = 0; k < n; k++) sb.append(lst[k]).append(' ');
-        System.out.print(sb);
-    }
-    static int read() throws IOException{
-        int c, n = System.in.read() & 15;
-        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
-        return n;
+        System.out.println(result);
     }
 }
