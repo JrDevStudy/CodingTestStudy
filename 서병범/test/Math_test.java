36ec9f94941b67cff5c0b81cbe1a75bf62dea84c
ByeongbumSeo <bum4321@naver.com>
2024-07-22 11:00:40 +0900

    서병범 7월 3주차 코딩테스트 스터디 1.baekjoon_1461 2.baekjoon_2011

---
+++ 서병범/test/Math_test.java
@@ -0,0 +1,17 @@
+package 서병범.test;
+
+public class Math_test {
+
+    public static void main(String[] args) {
+        int a = -10;
+        int absValue = Math.abs(a); // absValue = 10
+        /*
+        Math.abs() :
+        주어진 숫자의 절댓값을 반환합니다.
+        Math.abs()는 다양한 데이터 타입을 지원합니다: int, long, float, double.
+         */
+
+
+        System.out.println(absValue);
+    }
+}
